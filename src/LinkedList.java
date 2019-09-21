c602c07607b0676e7ffec053e84f96c58afb0650
dhikabotak99 <wicakbotak99@gmail.com>
2019-09-21 10:17:42 +0700

    Membuat method addLast

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -19,12 +19,28 @@ public class LinkedList {
 //    Method untuk menambah data ke awal
     public void addFirst(int data){
         Node newNode = new Node(data);
-        if (isEmpty()) {//Jika kosong
+        if (isEmpty()) {//Jika data kosong
             head=newNode;//Maka newNode masuk kedalam head
         }
-        else{//jika tidak kosong
+        else{//jika data tidak kosong
             newNode.next=head;//Maka head masuk ke next newNode
             head=newNode;//dan newNode masuk ke head
+        }
+    }
+    
+//    Method untuk menambah data ke akhir
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        Node temp = head;
+        if (isEmpty()) {//Jika data kosong
+            head=newNode;//Maka head di isi oleh newNode
+        }
+        else{//Jika data tidak kosong
+            while(temp.next!=null){//Maka dilakukan perulangan selama
+                                   //data di setelah data terakhir tidak kosong
+                temp=temp.next;//maka data terakhir di isi oleh pointer data terakhir
+            }
+            temp.next=newNode;//newNode mengisi data ke pointer setelah data terakhir
         }
     }
 }
