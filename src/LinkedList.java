c72127bfd3022d2572f74cf403b494c3537c502a
dhikabotak99 <wicakbotak99@gmail.com>
2019-09-21 10:06:57 +0700

    Membuat Method ShowAll

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -5,4 +5,14 @@
 public class LinkedList {
 //    Deklarasi Variabel
     Node head;
+    
+//    Method untuk menampilkan data
+    public void ShowAll(){
+        Node temp;
+        temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
 }
