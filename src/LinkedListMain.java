e99b35477292597b4803902c28649b2a6ef47460
dhikabotak99 <wicakbotak99@gmail.com>
2019-09-21 10:46:03 +0700

    Insert data in main

--- src/LinkedListMain.java
+++ src/LinkedListMain.java
@@ -3,5 +3,19 @@
  * @author Etem
  */
 public class LinkedListMain {
-    
+    public static void main(String[] args){
+        LinkedList ll = new LinkedList();
+        
+        ll.addFirst(58);
+        ll.addLast(4);
+        ll.addLast(98);
+        ll.addLast(102);
+        ll.addLast(22);
+        ll.addLast(88);
+        ll.addLast(12);
+        ll.deleteFirst();
+        ll.deleteFirst();
+        ll.deleteLast();
+        ll.ShowAll();
+    }
 }
