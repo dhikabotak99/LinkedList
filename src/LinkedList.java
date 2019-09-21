a5c22d2407c83de72dfd332c9bcdf7bbc4aa3c4a
dhikabotak99 <wicakbotak99@gmail.com>
2019-09-21 10:11:05 +0700

    Membuat method addFirst

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -15,4 +15,16 @@ public class LinkedList {
             temp=temp.next;
         }
     }
+    
+//    Method untuk menambah data ke awal
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if (isEmpty()) {//Jika kosong
+            head=newNode;//Maka newNode masuk kedalam head
+        }
+        else{//jika tidak kosong
+            newNode.next=head;//Maka head masuk ke next newNode
+            head=newNode;//dan newNode masuk ke head
+        }
+    }
 }
